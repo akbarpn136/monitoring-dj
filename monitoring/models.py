1ac6ea5673986972a51daa3e50739b8aba7fd96a
akbarpn136 <akbarpn136@gmail.com>
2016-02-23 14:43:25 +0700

    membuat migrasi model database

--- monitoring/models.py
+++ monitoring/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class DataAngin(models.Model):
-    tanggal = models.CharField(verbose_name='Tanggal Pengukuran', max_length=100)
-    waktu = models.CharField(verbose_name='Waktu Pengukuran', max_length=100)
+    tanggal = models.DateField(verbose_name='Tanggal Pengukuran')
+    waktu = models.TimeField(verbose_name='Waktu Pengukuran', max_length=100)
     arah = models.FloatField(verbose_name='Arah Angin (satuan sudut)')
     kompas = models.CharField(verbose_name='Simbol Arah (kompas)', max_length=3)
     kecepatan = models.FloatField(verbose_name='Kecepatan Angin (m/s)')
@@ -16,4 +16,4 @@ class DataAngin(models.Model):
     akselerator5 = models.FloatField(verbose_name='Akselerator 5')
 
     def __str__(self):
-        return self.tanggal + ' ' + str(self.grup_kecepatan)
+        return 'Grup kecepatan: ' + str(self.grup_kecepatan)
