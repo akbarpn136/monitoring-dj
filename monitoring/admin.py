1f8a81c2a247a2b0137f98d26bb39ead9332e27f
akbarpn136 <akbarpn136@gmail.com>
2016-02-25 09:59:14 +0700

    mendaftarkan model database kedalam admin panel

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import DaerahObjek, PilihanVisualisasi
 
 # Register your models here.
+admin.site.register(DaerahObjek)
+admin.site.register(PilihanVisualisasi)
