46a28e56363b9eedd43c84ba3cfd92832e665574
akbarpn136 <akbarpn136@gmail.com>
2016-02-25 12:12:51 +0700

    menambahkan fungsi untuk menampilkan menu monitoring berdasarkan daerah

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import DaerahObjek
 
 
 # Create your views here.
-def index(request):
-    pass
+def index(request, pk=None):
+    if pk is None:
+        return redirect('halaman_utama_pk', 1)
+    else:
+        data_daerah = DaerahObjek.objects.all()
+        data = {
+            'daerah': data_daerah
+        }
+
+    return render(request, 'master/base.html', data)
