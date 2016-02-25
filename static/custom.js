2561f5e8fc8730ee93431c289bdf7c1bae40ad4f
akbarpn136 <akbarpn136@gmail.com>
2016-02-25 12:10:38 +0700

    menambahkan fungsi popup untuk menu pengamatan

---
+++ static/custom.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sniper on 25/02/16.
+ */
+$(document).ready(function(){
+    $("a#browse").popup({
+        on: 'click',
+        inline: true,
+        position : 'bottom left'
+    });
+});
