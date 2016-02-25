579833224e77bd20866bd88a4dce25fc3e2085e2
akbarpn136 <akbarpn136@gmail.com>
2016-02-25 16:29:37 +0700

    menambahkan data baru kedalam halaman template

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import DaerahObjek, PilihanVisualisasi
+
+data_daerah = DaerahObjek.objects.all()
 
 
 # Create your views here.
@@ -8,11 +10,22 @@ def index(request, pk=None):
     if pk is None:
         return redirect('halaman_utama_pk', 1)
     else:
-        data_daerah = DaerahObjek.objects.all()
         data_visualisasi = PilihanVisualisasi.objects.filter(daerah=pk)
         data = {
             'daerah': data_daerah,
-            'visualisasi': data_visualisasi
+            'daerah_pk': pk,
+            'visualisasi': data_visualisasi,
         }
 
     return render(request, 'master/base.html', data)
+
+
+def visual(request, pk):
+    data_visual = get_object_or_404(PilihanVisualisasi, pk=pk)
+
+    data = {
+        'daerah': data_daerah,
+        'visual': data_visual,
+    }
+
+    return render(request, 'monitoring/visual.html', data)
