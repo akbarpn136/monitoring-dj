f0e6c0117998a6cd57f7ac3a4f34d359ee63bf5e
akbarpn136 <akbarpn136@gmail.com>
2016-02-25 16:08:29 +0700

    menambahkan data kedalam template

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import DaerahObjek
+from .models import DaerahObjek, PilihanVisualisasi
 
 
 # Create your views here.
@@ -9,8 +9,10 @@ def index(request, pk=None):
         return redirect('halaman_utama_pk', 1)
     else:
         data_daerah = DaerahObjek.objects.all()
+        data_visualisasi = PilihanVisualisasi.objects.filter(daerah=pk)
         data = {
-            'daerah': data_daerah
+            'daerah': data_daerah,
+            'visualisasi': data_visualisasi
         }
 
     return render(request, 'master/base.html', data)
