b4a0e9e31b8e84095e5a4c697d4efc98ff3f66b2
akbarpn136 <akbarpn136@gmail.com>
2016-02-25 16:29:12 +0700

    menambahkan pola url untuk jenis visualisasi data

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='halaman_utama'),
     url(r'^(?P<pk>[0-9]+)/$', views.index, name='halaman_utama_pk'),
+    url(r'^(?P<pk>[0-9]+)/visual/$', views.visual, name='halaman_visual'),
 ]
