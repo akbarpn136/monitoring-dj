a33ac5726d0edd119c30f004a208930d22731ec6
akbarpn136 <akbarpn136@gmail.com>
2016-02-26 15:52:22 +0700

    konfigurasi lanjut terhadap pengambilan data menjadi grafik beserta menambahkan fungsi objek JSON

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+from django.core import serializers
 
-from .models import DaerahObjek, PilihanVisualisasi
+from .models import DaerahObjek, PilihanVisualisasi, DataAngin
 
 data_daerah = DaerahObjek.objects.all()
 
 
 # Create your views here.
+def json_atr_angin(request, pk):
+    temp_output = serializers.serialize('json', DataAngin.objects.filter(daerah=pk),
+                                        fields=('tanggal', 'waktu', 'arah', 'kecepatan'))
+    print(temp_output)
+    return HttpResponse(temp_output, content_type='application/json')
+
+
 def index(request, pk=None):
     if pk is None:
         return redirect('halaman_utama_pk', 1)
@@ -23,9 +31,20 @@ def index(request, pk=None):
 def visual(request, pk):
     data_visual = get_object_or_404(PilihanVisualisasi, pk=pk)
 
+    if data_visual.jenis == 'ATR':
+        script = 'graph=document.getElementById("' + data_visual.jenis + '");' \
+                 'Plotly.plot( graph, [{' \
+                 'x: [1, 2, 3, 4, 5],' \
+                 'y: [1, 2, 4, 8, 16] }], {' \
+                 'margin: { t: 0 } } );'
+
+    else:
+        script = ''
+
     data = {
         'daerah': data_daerah,
         'visual': data_visual,
+        'script': script
     }
 
     return render(request, 'monitoring/visual.html', data)
