b9bfdc605b5ce84bee90e1acaf099d0fe46bd802
juaradagang.com <syareeefa@gmail.com>
2016-02-27 23:36:19 +0700

    menambahkan pola url baru untuk tautan JSON wind rose

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/atribut/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})$', views.json_atr_angin,
         name='json_angin'),
+    url(r'^(?P<pk>[0-9]+)/rose/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})$', views.json_rose_angin,
+        name='json_rose'),
 
     url(r'^$', views.index, name='halaman_utama'),
     url(r'^(?P<pk>[0-9]+)/$', views.index, name='halaman_utama_pk'),
