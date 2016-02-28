e6467782958bb4ad8b3fdc735260bebe8d05883f
juaradagang.com <syareeefa@gmail.com>
2016-02-28 17:20:11 +0700

    menambahkan pola url untuk menghasilkan data untuk pdf

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
         name='json_angin'),
     url(r'^(?P<pk>[0-9]+)/rose/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})$', views.json_rose_angin,
         name='json_rose'),
+    url(r'^(?P<pk>[0-9]+)/pdf/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})$', views.json_pdf_angin,
+        name='json_pdf'),
 
     url(r'^$', views.index, name='halaman_utama'),
     url(r'^(?P<pk>[0-9]+)/$', views.index, name='halaman_utama_pk'),
