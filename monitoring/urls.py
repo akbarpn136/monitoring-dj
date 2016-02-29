4ab005b9fe1138ea70e178b7a7749befd14d3afc
juaradagang.com <syareeefa@gmail.com>
2016-02-29 18:18:17 +0700

    menambahkan pola url untuk buat data JSON

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
         name='json_rose'),
     url(r'^(?P<pk>[0-9]+)/pdf/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})$', views.json_pdf_angin,
         name='json_pdf'),
+    url(r'^(?P<pk>[0-9]+)/pdf/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})$', views.json_wtr_angin,
+        name='json_wtr'),
 
     url(r'^$', views.index, name='halaman_utama'),
     url(r'^(?P<pk>[0-9]+)/$', views.index, name='halaman_utama_pk'),
