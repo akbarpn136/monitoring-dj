7aa76afff6a3c8234676fe82cd8e417d24932f45
juaradagang.com <syareeefa@gmail.com>
2016-03-01 15:58:48 +0700

    memperbaiki pola url dengan tambah dua variabel pembagian grup kecepatan yang diinginkan

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/atribut/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})$', views.json_atr_angin,
         name='json_angin'),
-    url(r'^(?P<pk>[0-9]+)/rose/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})$', views.json_rose_angin,
+    url(r'^(?P<pk>[0-9]+)/rose/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})/'
+        r'(?P<vmax>[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)/(?P<step>[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)/$', views.json_rose_angin,
         name='json_rose'),
     url(r'^(?P<pk>[0-9]+)/pdf/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})$', views.json_pdf_angin,
         name='json_pdf'),
