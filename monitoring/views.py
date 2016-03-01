8524daf5aa4d63377c5bedb1cfaabdec0c28aaa2
juaradagang.com <syareeefa@gmail.com>
2016-03-01 15:59:29 +0700

    menambahkan fungsi bantuan untuk menghasilkan warna hexadesimal beserta penambahan dua variabel pembagian grup kecepatan

--- monitoring/views.py
+++ monitoring/views.py
@@ -6,12 +6,17 @@ from scipy.stats import exponweib
 from .models import DaerahObjek, PilihanVisualisasi, DataAngin
 
 import json
+import random
 import numpy as np
 
 data_daerah = DaerahObjek.objects.all()
 
 
 # Create your views here.
+def gen_hex_colour_code():
+    return ''.join([random.choice('0123456789ABCDEF') for x in range(6)])
+
+
 def json_atr_angin(request, pk, dt_frm, dt_to):
     temp_output = serializers.serialize('json', DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm,
                                                                                            tanggal__lte=dt_to),
@@ -19,221 +24,33 @@ def json_atr_angin(request, pk, dt_frm, dt_to):
     return HttpResponse(temp_output, content_type='application/json')
 
 
-def json_rose_angin(request, pk, dt_frm, dt_to):
+def json_rose_angin(request, pk, dt_frm, dt_to, vmax=7, step=0.5):
     grp_v_tot = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to).count()
 
-    grp_v_0_05 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=0, grup_kecepatan__lt=0.5)
+    if grp_v_tot > 0:
+        k = {}
+        for lop in np.arange(0, float(vmax), float(step)):
+            grp_v_i = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
+                .filter(grup_kecepatan__gte=lop, grup_kecepatan__lt=lop + 0.5)
 
-    grp_v_0_05_ut = (grp_v_0_05.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_0_05_tl = (grp_v_0_05.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_0_05_tm = (grp_v_0_05.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_0_05_tg = (grp_v_0_05.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_0_05_sl = (grp_v_0_05.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_0_05_bd = (grp_v_0_05.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_0_05_br = (grp_v_0_05.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_0_05_bl = (grp_v_0_05.filter(kompas='BL').count() / grp_v_tot) * 100
+            grp_v_i_ut = (grp_v_i.filter(kompas='UT').count() / grp_v_tot) * 100
+            grp_v_i_tl = (grp_v_i.filter(kompas='TL').count() / grp_v_tot) * 100
+            grp_v_i_tm = (grp_v_i.filter(kompas='TM').count() / grp_v_tot) * 100
+            grp_v_i_tg = (grp_v_i.filter(kompas='TG').count() / grp_v_tot) * 100
+            grp_v_i_sl = (grp_v_i.filter(kompas='SL').count() / grp_v_tot) * 100
+            grp_v_i_bd = (grp_v_i.filter(kompas='BD').count() / grp_v_tot) * 100
+            grp_v_i_br = (grp_v_i.filter(kompas='BR').count() / grp_v_tot) * 100
+            grp_v_i_bl = (grp_v_i.filter(kompas='BL').count() / grp_v_tot) * 100
 
-    list_grp_v_0_05 = [grp_v_0_05_ut, grp_v_0_05_tl, grp_v_0_05_tm, grp_v_0_05_tg, grp_v_0_05_sl, grp_v_0_05_bd,
-                       grp_v_0_05_br, grp_v_0_05_bl]
+            list_grp_v_i = [grp_v_i_ut, grp_v_i_tl, grp_v_i_tm, grp_v_i_tg, grp_v_i_sl, grp_v_i_bd,
+                            grp_v_i_br, grp_v_i_bl, str(lop)+'-'+str(lop+0.5)+' m/s']
 
-    grp_v_05_1 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=0.5, grup_kecepatan__lt=1)
+            k[gen_hex_colour_code()] = list_grp_v_i
 
-    grp_v_05_1_ut = (grp_v_05_1.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_05_1_tl = (grp_v_05_1.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_05_1_tm = (grp_v_05_1.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_05_1_tg = (grp_v_05_1.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_05_1_sl = (grp_v_05_1.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_05_1_bd = (grp_v_05_1.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_05_1_br = (grp_v_05_1.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_05_1_bl = (grp_v_05_1.filter(kompas='BL').count() / grp_v_tot) * 100
+    else:
+        k = {}
 
-    list_grp_v_05_1 = [grp_v_05_1_ut, grp_v_05_1_tl, grp_v_05_1_tm, grp_v_05_1_tg, grp_v_05_1_sl, grp_v_05_1_bd,
-                       grp_v_05_1_br, grp_v_05_1_bl]
-
-    grp_v_1_15 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=1, grup_kecepatan__lt=1.5)
-
-    grp_v_1_15_ut = (grp_v_1_15.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_1_15_tl = (grp_v_1_15.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_1_15_tm = (grp_v_1_15.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_1_15_tg = (grp_v_1_15.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_1_15_sl = (grp_v_1_15.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_1_15_bd = (grp_v_1_15.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_1_15_br = (grp_v_1_15.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_1_15_bl = (grp_v_1_15.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_1_15 = [grp_v_1_15_ut, grp_v_1_15_tl, grp_v_1_15_tm, grp_v_1_15_tg, grp_v_1_15_sl, grp_v_1_15_bd,
-                       grp_v_1_15_br, grp_v_1_15_bl]
-
-    grp_v_15_2 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=1.5, grup_kecepatan__lt=2)
-
-    grp_v_15_2_ut = (grp_v_15_2.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_15_2_tl = (grp_v_15_2.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_15_2_tm = (grp_v_15_2.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_15_2_tg = (grp_v_15_2.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_15_2_sl = (grp_v_15_2.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_15_2_bd = (grp_v_15_2.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_15_2_br = (grp_v_15_2.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_15_2_bl = (grp_v_15_2.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_15_2 = [grp_v_15_2_ut, grp_v_15_2_tl, grp_v_15_2_tm, grp_v_15_2_tg, grp_v_15_2_sl, grp_v_15_2_bd,
-                       grp_v_15_2_br, grp_v_15_2_bl]
-
-    grp_v_2_25 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=2, grup_kecepatan__lt=2.5)
-
-    grp_v_2_25_ut = (grp_v_2_25.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_2_25_tl = (grp_v_2_25.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_2_25_tm = (grp_v_2_25.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_2_25_tg = (grp_v_2_25.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_2_25_sl = (grp_v_2_25.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_2_25_bd = (grp_v_2_25.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_2_25_br = (grp_v_2_25.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_2_25_bl = (grp_v_2_25.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_2_25 = [grp_v_2_25_ut, grp_v_2_25_tl, grp_v_2_25_tm, grp_v_2_25_tg, grp_v_2_25_sl, grp_v_2_25_bd,
-                       grp_v_2_25_br, grp_v_2_25_bl]
-
-    grp_v_25_3 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=2.5, grup_kecepatan__lt=3)
-
-    grp_v_25_3_ut = (grp_v_25_3.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_25_3_tl = (grp_v_25_3.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_25_3_tm = (grp_v_25_3.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_25_3_tg = (grp_v_25_3.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_25_3_sl = (grp_v_25_3.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_25_3_bd = (grp_v_25_3.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_25_3_br = (grp_v_25_3.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_25_3_bl = (grp_v_25_3.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_25_3 = [grp_v_25_3_ut, grp_v_25_3_tl, grp_v_25_3_tm, grp_v_25_3_tg, grp_v_25_3_sl, grp_v_25_3_bd,
-                       grp_v_25_3_br, grp_v_25_3_bl]
-
-    grp_v_3_35 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=3, grup_kecepatan__lt=3.5)
-
-    grp_v_3_35_ut = (grp_v_3_35.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_3_35_tl = (grp_v_3_35.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_3_35_tm = (grp_v_3_35.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_3_35_tg = (grp_v_3_35.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_3_35_sl = (grp_v_3_35.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_3_35_bd = (grp_v_3_35.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_3_35_br = (grp_v_3_35.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_3_35_bl = (grp_v_3_35.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_3_35 = [grp_v_3_35_ut, grp_v_3_35_tl, grp_v_3_35_tm, grp_v_3_35_tg, grp_v_3_35_sl, grp_v_3_35_bd,
-                       grp_v_3_35_br, grp_v_3_35_bl]
-
-    grp_v_35_4 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=3.5, grup_kecepatan__lt=4)
-
-    grp_v_35_4_ut = (grp_v_35_4.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_35_4_tl = (grp_v_35_4.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_35_4_tm = (grp_v_35_4.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_35_4_tg = (grp_v_35_4.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_35_4_sl = (grp_v_35_4.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_35_4_bd = (grp_v_35_4.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_35_4_br = (grp_v_35_4.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_35_4_bl = (grp_v_35_4.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_35_4 = [grp_v_35_4_ut, grp_v_35_4_tl, grp_v_35_4_tm, grp_v_35_4_tg, grp_v_35_4_sl, grp_v_35_4_bd,
-                       grp_v_35_4_br, grp_v_35_4_bl]
-
-    grp_v_4_45 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=4, grup_kecepatan__lt=4.5)
-
-    grp_v_4_45_ut = (grp_v_4_45.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_4_45_tl = (grp_v_4_45.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_4_45_tm = (grp_v_4_45.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_4_45_tg = (grp_v_4_45.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_4_45_sl = (grp_v_4_45.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_4_45_bd = (grp_v_4_45.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_4_45_br = (grp_v_4_45.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_4_45_bl = (grp_v_4_45.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_4_45 = [grp_v_4_45_ut, grp_v_4_45_tl, grp_v_4_45_tm, grp_v_4_45_tg, grp_v_4_45_sl, grp_v_4_45_bd,
-                       grp_v_4_45_br, grp_v_4_45_bl]
-
-    grp_v_45_5 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=4.5, grup_kecepatan__lt=5)
-
-    grp_v_45_5_ut = (grp_v_45_5.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_45_5_tl = (grp_v_45_5.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_45_5_tm = (grp_v_45_5.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_45_5_tg = (grp_v_45_5.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_45_5_sl = (grp_v_45_5.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_45_5_bd = (grp_v_45_5.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_45_5_br = (grp_v_45_5.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_45_5_bl = (grp_v_45_5.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_45_5 = [grp_v_45_5_ut, grp_v_45_5_tl, grp_v_45_5_tm, grp_v_45_5_tg, grp_v_45_5_sl, grp_v_45_5_bd,
-                       grp_v_45_5_br, grp_v_45_5_bl]
-
-    grp_v_5_55 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=5, grup_kecepatan__lt=5.5)
-
-    grp_v_5_55_ut = (grp_v_5_55.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_5_55_tl = (grp_v_5_55.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_5_55_tm = (grp_v_5_55.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_5_55_tg = (grp_v_5_55.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_5_55_sl = (grp_v_5_55.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_5_55_bd = (grp_v_5_55.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_5_55_br = (grp_v_5_55.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_5_55_bl = (grp_v_5_55.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_5_55 = [grp_v_5_55_ut, grp_v_5_55_tl, grp_v_5_55_tm, grp_v_5_55_tg, grp_v_5_55_sl, grp_v_5_55_bd,
-                       grp_v_5_55_br, grp_v_5_55_bl]
-
-    grp_v_55_6 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=5.5, grup_kecepatan__lt=6)
-
-    grp_v_55_6_ut = (grp_v_55_6.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_55_6_tl = (grp_v_55_6.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_55_6_tm = (grp_v_55_6.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_55_6_tg = (grp_v_55_6.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_55_6_sl = (grp_v_55_6.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_55_6_bd = (grp_v_55_6.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_55_6_br = (grp_v_55_6.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_55_6_bl = (grp_v_55_6.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_55_6 = [grp_v_55_6_ut, grp_v_55_6_tl, grp_v_55_6_tm, grp_v_55_6_tg, grp_v_55_6_sl, grp_v_55_6_bd,
-                       grp_v_55_6_br, grp_v_55_6_bl]
-
-    grp_v_6_60 = DataAngin.objects.filter(daerah=pk).filter(tanggal__gte=dt_frm, tanggal__lte=dt_to) \
-        .filter(grup_kecepatan__gte=6)
-
-    grp_v_6_60_ut = (grp_v_6_60.filter(kompas='UT').count() / grp_v_tot) * 100
-    grp_v_6_60_tl = (grp_v_6_60.filter(kompas='TL').count() / grp_v_tot) * 100
-    grp_v_6_60_tm = (grp_v_6_60.filter(kompas='TM').count() / grp_v_tot) * 100
-    grp_v_6_60_tg = (grp_v_6_60.filter(kompas='TG').count() / grp_v_tot) * 100
-    grp_v_6_60_sl = (grp_v_6_60.filter(kompas='SL').count() / grp_v_tot) * 100
-    grp_v_6_60_bd = (grp_v_6_60.filter(kompas='BD').count() / grp_v_tot) * 100
-    grp_v_6_60_br = (grp_v_6_60.filter(kompas='BR').count() / grp_v_tot) * 100
-    grp_v_6_60_bl = (grp_v_6_60.filter(kompas='BL').count() / grp_v_tot) * 100
-
-    list_grp_v_6_60 = [grp_v_6_60_ut, grp_v_6_60_tl, grp_v_6_60_tm, grp_v_6_60_tg, grp_v_6_60_sl, grp_v_6_60_bd,
-                       grp_v_6_60_br, grp_v_6_60_bl]
-
-    obj = [{
-        'trace1': list_grp_v_0_05,
-        'trace2': list_grp_v_05_1,
-        'trace3': list_grp_v_1_15,
-        'trace4': list_grp_v_15_2,
-        'trace5': list_grp_v_2_25,
-        'trace6': list_grp_v_25_3,
-        'trace7': list_grp_v_3_35,
-        'trace8': list_grp_v_35_4,
-        'trace9': list_grp_v_4_45,
-        'trace10': list_grp_v_45_5,
-        'trace11': list_grp_v_5_55,
-        'trace12': list_grp_v_55_6,
-        'trace13': list_grp_v_6_60,
-    }]
-
-    return HttpResponse(json.dumps(obj), content_type='application/json')
+    return HttpResponse(json.dumps(k), content_type='application/json')
 
 
 def json_pdf_angin(request, pk, dt_frm, dt_to):
