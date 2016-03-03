88e5ec737b69430b493b4b998d1fd233273b2efe
juaradagang.com <syareeefa@gmail.com>
2016-03-03 11:36:38 +0700

    menambahkan parameter baru arah angin kedalam pola url water fall

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/pdf/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})$', views.json_pdf_angin,
         name='json_pdf'),
     url(r'^(?P<pk>[0-9]+)/wtr/(?P<dt_frm>\d{4}-\d{2}-\d{2})/(?P<dt_to>\d{4}-\d{2}-\d{2})/'
-        r'(?P<grup>[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)/(?P<step>[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)/$', views.json_wtr_angin,
-        name='json_wtr'),
+        r'(?P<grup>[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)/(?P<step>[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)/'
+        r'(?P<kompas>[\w]+)/$', views.json_wtr_angin, name='json_wtr'),
 
     url(r'^$', views.index, name='halaman_utama'),
     url(r'^(?P<pk>[0-9]+)/$', views.index, name='halaman_utama_pk'),
