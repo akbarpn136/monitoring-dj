e73cb93afe9c2e59a47bfdd60b0a36a3338bcbc3
akbarpn136 <akbarpn136@gmail.com>
2016-04-12 11:10:23 +0700

    menambahkan input baru kedalam form berupa pemilihan arah angin

--- static/atribut_rms.js
+++ static/atribut_rms.js
@@ -19,33 +19,50 @@ $(document).ready(function(){
 
              $("input#grup_v").change(function(){
                  var grup_v_val = $(this).val();
+                 var selektor_kompas = '<select id="slk_kmps" class="ui dropdown">';
+                    selektor_kompas += '<option value="all">Semua arah angin</option>';
+                    selektor_kompas += '<option value="UT">Utara</option>';
+                    selektor_kompas += '<option value="TL">Timur Laut</option>';
+                    selektor_kompas += '<option value="TM">Timur</option>';
+                    selektor_kompas += '<option value="TG">Tenggara</option>';
+                    selektor_kompas += '<option value="SL">Selatan</option>';
+                    selektor_kompas += '<option value="BD">Barat Daya</option>';
+                    selektor_kompas += '<option value="BR">Barat</option>';
+                    selektor_kompas += '<option value="BL">Barat Laut</option>';
+                    selektor_kompas += '</select>';
 
-                 $("div#loader").addClass('active');
+                    $("div#jenis_grafik").append().html(selektor_kompas);
+                    $("select#slk_kmps").dropdown({
+                        placeholder: false,
+                        onChange: function(slk_kmps_val){
+                            $("div#loader").addClass('active');
 
-                 $.get(link_rms+vmax_val+'/'+grup_v_val, function(nilai_rms_json){
-                     var graph1=document.getElementById("RMS");
-                     var trace1 = {
-                         x: nilai_rms_json['data_x'],
-                         y: nilai_rms_json['data_y'],
-                         fill: 'tozeroy',
-                         type: 'scatter',
-                         showlegend: false
-                     };
-                     var layout1 = {
-                         title: 'Grafik RMS Getaran',
-                         xaxis: {
-                             title: "Kelompok Kecepatan (m/s)"
-                         },
-                         yaxis: {
-                             title: "RMS Getaran"
-                         }
-                     };
+                            $.get(link_rms+vmax_val+'/'+grup_v_val+'/'+slk_kmps_val, function(nilai_rms_json){
+                                 var graph1=document.getElementById("RMS");
+                                 var trace1 = {
+                                     x: nilai_rms_json['data_x'],
+                                     y: nilai_rms_json['data_y'],
+                                     fill: 'tozeroy',
+                                     type: 'scatter',
+                                     showlegend: false
+                                 };
+                                 var layout1 = {
+                                     title: 'Grafik RMS Getaran',
+                                     xaxis: {
+                                         title: "Kelompok Kecepatan (m/s)"
+                                     },
+                                     yaxis: {
+                                         title: "RMS Getaran"
+                                     }
+                                 };
 
-                     var data = [trace1];
-                     $("div#loader").removeClass('active');
-                     Plotly.newPlot(graph1, data, layout1, {showLink: false, displaylogo: false, scrollZoom: true});
-                     Plotly.redraw(graph1);
-                 });
+                                 var data = [trace1];
+                                 $("div#loader").removeClass('active');
+                                 Plotly.newPlot(graph1, data, layout1, {showLink: false, displaylogo: false, scrollZoom: true});
+                                 Plotly.redraw(graph1);
+                             });
+                        }
+                    });
              });
         }
     });
