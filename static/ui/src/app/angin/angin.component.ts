548842ad23ed4c5261c850666bc069c133f19ee0
akbarpn136 <akbarpn136@gmail.com>
2017-03-16 14:44:01 +0700

    menambahkan komponen awal untuk tampil data angin

---
+++ static/ui/src/app/angin/angin.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'ui-angin',
+    templateUrl: './angin.component.html',
+    styleUrls: ['./angin.component.css']
+})
+export class AnginComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
