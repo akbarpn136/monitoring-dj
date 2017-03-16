2342d2179fafe30f1aeab17e65337c55a4951b77
akbarpn136 <akbarpn136@gmail.com>
2017-03-16 14:45:38 +0700

    menambahkan route, komponen, dan directive

--- static/ui/src/app/app.module.ts
+++ static/ui/src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {route} from './app.route';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {MunculiniDirective} from "./directives/munculini.directive";
+import {MunculituDirective} from "./directives/munculitu.directive";
+import { AnginComponent } from './angin/angin.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MunculiniDirective,
+        MunculituDirective,
+        HeaderComponent,
+        AnginComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        route
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
