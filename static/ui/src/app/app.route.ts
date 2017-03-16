5ceb76d9f3d4f2236cd5c7fd06bf4d279d892ebf
akbarpn136 <akbarpn136@gmail.com>
2017-03-16 14:48:42 +0700

    menambahkan beberapa definis route awal

---
+++ static/ui/src/app/app.route.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by USER on 2/20/2017.
+ */
+import {Route, RouterModule} from '@angular/router';
+import {AnginComponent} from "./angin/angin.component";
+
+const APP_ROUTES: Route[] = [
+    {path: 'rms', component: AnginComponent},
+    {path: 'waterfall', component: AnginComponent},
+    {path: 'pdf', component: AnginComponent},
+    {path: 'windrose', component: AnginComponent},
+    {path: 'angin', component: AnginComponent},
+    {path: '', component: AnginComponent},
+    {path: '**', redirectTo:'', pathMatch:'full'},
+];
+
+export const route = RouterModule.forRoot(APP_ROUTES);
