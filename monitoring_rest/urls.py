3026f968acbdec0a3fccf1137110574a3636f748
akbarpn136 <akbarpn136@gmail.com>
2017-03-17 15:45:23 +0700

    membuat route backend untuk respon objek json

--- monitoring_rest/urls.py
+++ monitoring_rest/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^angin/(?P<date_from>\d{4}-\d{2}-\d{2})/(?P<date_to>\d{4}-\d{2}-\d{2})/$',
         views.MonitorAngin.as_view(),
         name='atribut_angin'),
+    url(r'^windrose/(?P<date_from>\d{4}-\d{2}-\d{2})/(?P<date_to>\d{4}-\d{2}-\d{2})/',
+        views.MonitorWindrose.as_view(),
+        name='windrose_angin'),
 ]
