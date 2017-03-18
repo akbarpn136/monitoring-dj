2385a2eb6a9a7321ce3ce2b3a1c5e57deefcd33c
akbarpn136 <akbarpn136@gmail.com>
2017-03-18 15:54:24 +0700

    tambah route untuk RMS

--- monitoring_rest/urls.py
+++ monitoring_rest/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^rms/$', views.MonitorRMS.as_view(), name='rms_angin'),
     url(r'^angin/(?P<date_from>\d{4}-\d{2}-\d{2})/(?P<date_to>\d{4}-\d{2}-\d{2})/$',
         views.MonitorAngin.as_view(),
         name='atribut_angin'),
-    url(r'^windrose/(?P<date_from>\d{4}-\d{2}-\d{2})/(?P<date_to>\d{4}-\d{2}-\d{2})/',
+    url(r'^windrose/(?P<date_from>\d{4}-\d{2}-\d{2})/(?P<date_to>\d{4}-\d{2}-\d{2})/$',
         views.MonitorWindrose.as_view(),
         name='windrose_angin'),
 ]
