ee3da0443b51fc63ff76abb2b8ed514a58ac852c
akbarpn136 <akbarpn136@gmail.com>
2017-03-18 15:54:45 +0700

    menambahkan class untuk fungsi proses data RMS

--- monitoring_rest/views.py
+++ monitoring_rest/views.py
@@ -41,6 +41,11 @@ class MonitorWindrose(ListView):
     def get(self, request, *args, **kwargs):
         vmax = float(self.request.GET.get('vmax'))
         step = float(self.request.GET.get('step'))
+        obj = self.do_task(vmax, step)
+
+        return HttpResponse(json.dumps(obj, sort_keys=True), content_type='Applications/json')
+
+    def do_task(self, vmax, step):
         v_range = numpy.arange(0.0, vmax + step, step).tolist()
         total = self.get_queryset().count()
 
@@ -70,6 +75,56 @@ class MonitorWindrose(ListView):
                                   'Barat Laut'],
                    } for i, v in enumerate(v_range)]
         else:
-            obj = {}
 
-        return HttpResponse(json.dumps(obj, sort_keys=True), content_type='Applications/json')
+            obj = [{}]
+
+        return obj
+
+
+class MonitorRMS(ListView):
+    def get(self, request, *args, **kwargs):
+        vmax = float(self.request.GET.get('vmax'))
+        step = float(self.request.GET.get('step'))
+        arah = self.request.GET.get('arah')
+        obj = self.do_task(vmax, step, arah)
+
+        return HttpResponse(json.dumps([obj], sort_keys=True), content_type='Applications/json')
+
+    @staticmethod
+    def do_task(vmax, step, arah):
+        v_range = numpy.arange(0.0, vmax, step).tolist()
+        obj = {}
+        data_x = []
+        data_y = []
+
+        for i, v in enumerate(v_range):
+            v = round(v, 1)
+
+            if arah == 'all':
+                data_acc = models.DataAngin.objects.filter(
+                    kecepatan__gte=v,
+                    kecepatan__lt=v + step
+                ).values_list(
+                    'akselerator1', 'akselerator2', 'akselerator3', 'akselerator4', 'akselerator5'
+                )
+            else:
+                data_acc = models.DataAngin.objects.filter(
+                    kecepatan__gte=v,
+                    kecepatan__lt=v + step,
+                    kompas__contains=arah
+                ).values_list(
+                    'akselerator1', 'akselerator2', 'akselerator3', 'akselerator4', 'akselerator5'
+                )
+
+            numpy_data_acc = numpy.array(data_acc)
+            numpy_data_acc_square = numpy.square(numpy_data_acc)
+            numpy_data_acc_mean = numpy.mean(numpy_data_acc_square)
+            numpy_data_acc_root = numpy.sqrt(numpy_data_acc_mean)
+
+            data_x.append(str(v) + ' - ' + str(v+step))
+            data_y.append(numpy_data_acc_root)
+
+        obj['x'] = data_x
+        obj['y'] = data_y
+
+        return obj
