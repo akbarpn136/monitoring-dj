407a66abcc53dda82b448528c144cf1ea1b1cc9d
akbarpn136 <akbarpn136@gmail.com>
2017-03-19 20:00:00 +0700

    menambahkan service untuk ambil data waterfall

--- static/ui/src/app/services/proses.service.ts
+++ static/ui/src/app/services/proses.service.ts
@@ -10,6 +10,7 @@ export class ProsesService {
     private URL_ANGIN = 'http://localhost:8000/v1/monitor/angin/';
     private URL_WINDROSE = 'http://localhost:8000/v1/monitor/windrose/';
     private URL_RMS = 'http://localhost:8000/v1/monitor/rms/';
+    private URL_WATERFALL = 'http://localhost:8000/v1/monitor/waterfall/';
 
     constructor(private http: Http) {
     }
@@ -74,6 +75,42 @@ export class ProsesService {
         let options = new RequestOptions({headers: header, search: parameter});
 
         return this.http.get(`${this.URL_RMS}`, options)
+            .map(
+                (res: Response) => {
+                    return res.json();
+                }
+            )
+            .catch(
+                (err: Response | any) => {
+                    if (err instanceof Response) {
+                        return Observable.throw(err.json());
+                    }
+
+                    else {
+                        return err.message;
+                    }
+                }
+            );
+    }
+
+    ambilWaterfallAngin(date_from, date_to, vmax, step, arah, wkt_awal, wkt_akhir) {
+        let header = new Headers({'Content-Type': 'application/json'});
+        let parameter = new URLSearchParams();
+        parameter.set('vmax', vmax);
+        parameter.set('step', step);
+        parameter.set('arah', arah);
+
+        if (!wkt_awal) {
+            parameter.set('wkt_awal', '00:00');
+        }
+
+        if (!wkt_akhir) {
+            parameter.set('wkt_akhir', '23:59');
+        }
+
+        let options = new RequestOptions({headers: header, search: parameter});
+
+        return this.http.get(`${this.URL_WATERFALL}${date_from}/${date_to}/`, options)
             .map(
                 (res: Response) => {
                     return res.json();
