f422b763af93a8e0ba965ea7b4919b42901f12cd
akbarpn136 <akbarpn136@gmail.com>
2017-03-22 11:40:04 +0700

    membuat fungsi extra untuk melakukan FFT signal

--- monitoring_rest/extras.py
+++ monitoring_rest/extras.py
@@ -1,3 +1,4 @@
+import numpy
 from scipy import signal as sig
 
 
@@ -15,3 +16,42 @@ class DoFilter(object):
         y = sig.lfilter(a[0], a[1], self.data)
 
         return y
+
+
+class DoFFT(object):
+    def __init__(self, data, sampel_spacing, first_cutoff, second_cutoff, order):
+        self.data = data
+        self.sample_spacing = sampel_spacing
+        self.first_cutoff = first_cutoff
+        self.second_cutoff = second_cutoff
+        self.order = order
+        super(DoFFT, self).__init__()
+
+    def make_fft(self):
+        # Data akselerometer
+        numpy_data_acc = numpy.array(self.data).flatten()
+
+        # Jumlah data
+        N = numpy_data_acc.size
+
+        # Sample spacing
+        T = self.sample_spacing
+
+        if N > 0:
+            # FFT
+            fltr = DoFilter(data=numpy_data_acc).butter_bandpass_filter(self.first_cutoff,
+                                                                        self.second_cutoff,
+                                                                        1 / T,
+                                                                        order=self.order)
+            zf = numpy.fft.fft(fltr)
+            m = numpy.absolute(zf[:N // 2])
+            yf = numpy.fft.fftfreq(N, d=T).tolist()
+
+            y = yf[:N // 2]
+            z = m.tolist()
+
+        else:
+            y = numpy.zeros(N//2).tolist()
+            z = numpy.zeros(N//2).tolist()
+
+        return N, y, z
