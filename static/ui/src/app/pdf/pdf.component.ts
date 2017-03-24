5cfc51867b65eeb766bd9f92c0f0dc967cc4aaa3
akbarpn136 <akbarpn136@gmail.com>
2017-03-24 11:17:24 +0700

    menambahkan komponen baru untuk pdf angin

---
+++ static/ui/src/app/pdf/pdf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ui-pdf',
+  templateUrl: './pdf.component.html',
+  styleUrls: ['./pdf.component.css']
+})
+export class PdfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
