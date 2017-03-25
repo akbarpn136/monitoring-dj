96443d5bb33f22bf0c640d3b65071c23393e09c4
akbarpn136 <akbarpn136@gmail.com>
2017-03-25 19:09:26 +0700

    membuat fungsi untuk membuat reactive form

--- static/ui/src/app/login/login.component.ts
+++ static/ui/src/app/login/login.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'ui-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'ui-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    private loginForm: FormGroup;
 
-  constructor() { }
+    constructor(private fb: FormBuilder) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.createFormLogin();
+    }
 
+    createFormLogin() {
+
+        this.loginForm = this.fb.group({
+            username: this.fb.control('', Validators.compose([Validators.required])),
+            password: this.fb.control('', Validators.compose([Validators.required])),
+        });
+    }
+
+    onLoginSubmit(data) {
+        console.log(data);
+    }
 }
