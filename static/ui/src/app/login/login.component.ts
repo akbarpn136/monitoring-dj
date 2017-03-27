d5a36c79517891fb3068184cdac58af50414811c
akbarpn136 <akbarpn136@gmail.com>
2017-03-28 06:51:11 +0700

    membuat fungsi untuk otentikasi saat submit dan cek validasi user

--- static/ui/src/app/login/login.component.ts
+++ static/ui/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../services/auth.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
     selector: 'ui-login',
@@ -8,8 +10,23 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class LoginComponent implements OnInit {
     private loginForm: FormGroup;
+    private isError: boolean = false;
+    private isShow: boolean = false;
+    private key: any;
+    private message: any;
 
-    constructor(private fb: FormBuilder) {
+    constructor(private fb: FormBuilder,
+                private auth: AuthService,
+                private activ: ActivatedRoute,
+                private route: Router) {
+        this.activ.params.subscribe(
+            () => {
+                if (localStorage.getItem('qwerty')) {
+                    //noinspection JSIgnoredPromiseFromCall
+                    route.navigate(['']);
+                }
+            }
+        );
     }
 
     ngOnInit() {
@@ -25,6 +42,28 @@ export class LoginComponent implements OnInit {
     }
 
     onLoginSubmit(data) {
-        console.log(data);
+        let u = data['username'];
+        let p = data['password'];
+
+        this.isShow = true;
+        this.auth.cobaLogin(u, p).subscribe(
+            (body) => {
+                this.isError = false;
+
+                localStorage.setItem('qwerty', body['token']);
+                localStorage.setItem('user', body['name']);
+                localStorage.setItem('super', body['isSuper']);
+                //noinspection JSIgnoredPromiseFromCall
+                this.route.navigate(['']);
+                this.isShow = false;
+            },
+
+            err => {
+                this.isError = true;
+                this.isShow = false;
+                this.key = Object.keys(err);
+                this.message = err;
+            }
+        );
     }
 }
