c594718f360b8a1546f486f02f847508f40cb842
akbarpn136 <akbarpn136@gmail.com>
2017-03-28 06:39:15 +0700

    membuat servis guard dengan metoda sederhana atau melalui validasi server

--- static/ui/src/app/services/guard.service.ts
+++ static/ui/src/app/services/guard.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {CanActivate, Router} from "@angular/router";
+import {AuthService} from "./auth.service";
 
 @Injectable()
-export class GuardService {
+export class GuardService implements CanActivate {
+    constructor(private auth: AuthService, private router: Router) {
+    }
 
-  constructor() { }
+    canActivate() {
+        return this.checkValidSimple();
+    }
 
+    checkValid() {
+        return this.auth.isValid()
+            .map(
+                res => {
+                    if (!res) {
+                        //noinspection JSIgnoredPromiseFromCall
+                        this.router.navigate(['login']);
+                        return false
+                    }
+                    return res
+                }
+            );
+    }
+
+    checkValidSimple() {
+        let qwerty = localStorage.getItem('qwerty');
+
+        if (qwerty) {
+            return true;
+        }
+        else {
+            //noinspection JSIgnoredPromiseFromCall
+            this.router.navigate(['login']);
+            return false
+        }
+    }
 }
