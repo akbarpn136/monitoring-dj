94e3b8cbfbef4c0ce075965928106eb6bcfd146b
akbarpn136 <akbarpn136@gmail.com>
2017-03-30 10:14:48 +0700

    melakukan registrasi modul datepicker kedalam modul aplikasi utama

--- static/ui/src/app/app.module.ts
+++ static/ui/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {ReactiveFormsModule} from '@angular/forms';
 import {route} from './app.route';
+import {MyDatePickerModule} from 'mydatepicker';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
@@ -12,10 +13,10 @@ import {MunculituDirective} from "./directives/munculitu.directive";
 import {AnginComponent} from './angin/angin.component';
 import {SpinnerComponent} from './spinner/spinner.component';
 import {WindroseComponent} from './windrose/windrose.component';
-import { RmsComponent } from './rms/rms.component';
-import { WaterfallComponent } from './waterfall/waterfall.component';
-import { PdfComponent } from './pdf/pdf.component';
-import { LoginComponent } from './login/login.component';
+import {RmsComponent} from './rms/rms.component';
+import {WaterfallComponent} from './waterfall/waterfall.component';
+import {PdfComponent} from './pdf/pdf.component';
+import {LoginComponent} from './login/login.component';
 import {GuardService} from "./services/guard.service";
 import {AuthService} from "./services/auth.service";
 
@@ -38,7 +39,8 @@ import {AuthService} from "./services/auth.service";
         FormsModule,
         ReactiveFormsModule,
         HttpModule,
-        route
+        route,
+        MyDatePickerModule
     ],
     providers: [AuthService, GuardService],
     bootstrap: [AppComponent]
